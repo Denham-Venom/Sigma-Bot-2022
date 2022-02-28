237b62650013df10ce3e174b8f78d6f1a570e90b
Tatertot33 <56095115+Tatertot33@users.noreply.github.com>
2022-02-28 14:30:03 -0600

    started implementing this

--- src/main/java/frc/lib/util/SwerveCommandRotation2dSupplierGenerator.java
+++ src/main/java/frc/lib/util/SwerveCommandRotation2dSupplierGenerator.java
@@ -4,18 +4,43 @@
 
 package frc.lib.util;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.Trajectory.State;
+import edu.wpi.first.wpilibj.Timer;
 
 /** Add your docs here. */
 public class SwerveCommandRotation2dSupplierGenerator {
 
     public static Supplier<Rotation2d> generateSwerveCommandRotation2dSupplier(Trajectory trajectory, List<Pose2d> waypoints) {
+        ArrayList<Double> times = new ArrayList<>();
+        List<State> trajStates = trajectory.getStates();
+        int numStates = trajStates.size();
+        for(int i = 0, waypointIndex = 0; i < numStates; i++) {
+            
+        }
 
         return () -> new Rotation2d();
     }
+
+    // private static Supplier<Rotation2d> getSupplier() {
+    //     Timer timer = new Timer();
+    //     boolean timerStarted = false;
+    //     //Supplier<Rotation2d> -- returns the next specified heading of the robot in the path
+    //     Supplier<Rotation2d> ang = () -> {
+    //         if(!timerStarted) {
+    //             timer.reset();
+    //             timer.start();
+    //         }
+    //         double curTime = timer.get();
+    //         return traj.sample(curTime).poseMeters.getRotation();
+    //     };
+
+    //     return ang;
+    // }
 }
