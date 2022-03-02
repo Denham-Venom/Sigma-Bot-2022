03ee07870f56b9f10c1ff39eb9587771204e8267
BreyaD <56096105+BreyaD@users.noreply.github.com>
2022-03-02 16:40:23 -0600

    Finished implementing SwerveCommandRotation2dSupplierGenerator and helper class

--- src/main/java/frc/lib/util/SwerveCommandRotation2dSupplierGenerator.java
+++ src/main/java/frc/lib/util/SwerveCommandRotation2dSupplierGenerator.java
@@ -12,35 +12,30 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.Trajectory.State;
-import edu.wpi.first.wpilibj.Timer;
 
 /** Add your docs here. */
 public class SwerveCommandRotation2dSupplierGenerator {
 
-    public static Supplier<Rotation2d> generateSwerveCommandRotation2dSupplier(Trajectory trajectory, List<Pose2d> waypoints) {
-        ArrayList<Double> times = new ArrayList<>();
-        List<State> trajStates = trajectory.getStates();
-        int numStates = trajStates.size();
-        for(int i = 0, waypointIndex = 0; i < numStates; i++) {
-            
-        }
+  public static Supplier<Rotation2d> generateSwerveCommandRotation2dSupplier(Trajectory trajectory, List<Pose2d> waypoints) {
+      ArrayList<Double> times = new ArrayList<>();
+      //first waypoint should occur at time zero so skip
+      times.add(0.);
 
-        return () -> new Rotation2d();
-    }
+      //loop over states and find points where each waypoints occur TODO - check if waypoints are guaranteed to appear in a trajectory exactly
+      List<State> trajStates = trajectory.getStates();
+      int numStates = trajStates.size();
+      Translation2d curTr = waypoints.get(1).getTranslation();
+      for(int i = 0, waypointIndex = 1; i < numStates-1; i++) {
+          State state = trajStates.get(i);
+          if(state.poseMeters.getTranslation().equals(curTr)) {
+              waypointIndex++;
+              curTr = waypoints.get(waypointIndex).getTranslation();
+              times.add(state.timeSeconds);
+          }
+      }
+      //last waypoint should be last state of trajectory
+      times.add(trajStates.get(numStates-1).timeSeconds);
 
-    // private static Supplier<Rotation2d> getSupplier() {
-    //     Timer timer = new Timer();
-    //     boolean timerStarted = false;
-    //     //Supplier<Rotation2d> -- returns the next specified heading of the robot in the path
-    //     Supplier<Rotation2d> ang = () -> {
-    //         if(!timerStarted) {
-    //             timer.reset();
-    //             timer.start();
-    //         }
-    //         double curTime = timer.get();
-    //         return traj.sample(curTime).poseMeters.getRotation();
-    //     };
-
-    //     return ang;
-    // }
+      return new SwerveAngleSupplier(times, waypoints);
+  }
 }
