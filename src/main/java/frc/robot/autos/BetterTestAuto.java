b7cfe32646736791e720d5169f263a1013dbdcda
Callie1234 <86382936+Callie1234@users.noreply.github.com>
2022-03-04 18:50:54 -0600

    made auto to test double supplier swerve

--- src/main/java/frc/robot/autos/BetterTestAuto.java
+++ src/main/java/frc/robot/autos/BetterTestAuto.java
@@ -5,16 +5,19 @@
 package frc.robot.autos;
 
 import java.util.List;
+import java.util.function.Supplier;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
-import frc.lib.util.HolonomicTrajectoryGenerator;
+import frc.lib.util.SwerveCommandRotation2dSupplierGenerator;
 import frc.robot.Constants;
-import frc.robot.Constants.AutoConstants;
 import frc.robot.subsystems.Swerve;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
@@ -24,22 +27,28 @@ public class BetterTestAuto extends SequentialCommandGroup {
   /** Creates a new BetterTestAuto. */
   public BetterTestAuto(Swerve s_Swerve) {
 
-    Pose2d startPos = AutoConstants.startPos;
-
     List<Pose2d> points = List.of(
-      startPos, 
-      AutoConstants.rightPoints[0]
+      new Pose2d(3.896, 1.391, new Rotation2d(0)),
+      new Pose2d(5.979, 1.462, new Rotation2d(-1.593)),
+      new Pose2d(7.788, 1.426, new Rotation2d(0))
     );
-    Trajectory traj = HolonomicTrajectoryGenerator.generateTrajectory(points, Constants.Swerve.trajectoryConfig);
-    SwerveControllerCommand test = HolonomicTrajectoryGenerator.generateSwerveControllerCommand(
-      points,
+
+    Trajectory traj = TrajectoryGenerator.generateTrajectory(points, Constants.Swerve.trajectoryConfig);
+    Supplier<Rotation2d> rot = SwerveCommandRotation2dSupplierGenerator.generateSwerveCommandRotation2dSupplier(traj, points);
+    SwerveControllerCommand move = new SwerveControllerCommand(
       traj,
-      s_Swerve
-      );
-    
+      s_Swerve::getPose,
+      Constants.Swerve.swerveKinematics,
+      new PIDController(Constants.AutoConstants.kPXController, 0, 0),
+      new PIDController(Constants.AutoConstants.kPYController, 0, 0),
+      new ProfiledPIDController(Constants.AutoConstants.kPThetaController, 0, 0, Constants.AutoConstants.kThetaControllerConstraints),
+      rot,
+      s_Swerve::setModuleStates,
+      s_Swerve);
+
     addCommands(
       new InstantCommand(() -> s_Swerve.resetOdometry(traj.getInitialPose())),
-      test
+      move
       );
     
   }
